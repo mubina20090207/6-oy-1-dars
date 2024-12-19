8821e40cfb642cd8ffdb343458cf1caac5edd674
mubina20090207 <abduraxmonovamubina02@gmail.com>
2024-12-18 16:53:46 -0800

    uy ishi

---
+++ project_name/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
